d81d7aa8531fcd95c6e1ab32e5641082c25d00d2
Dmitriy <d.zhiharev83@gmail.com>
2017-08-25 15:54:30 +0300

    Исправлены некоторые проблемы при вводе количества товара.

--- app/src/main/java/com/RightDirection/ShoppingList/utils/DecimalDigitsInputFilter.java
+++ app/src/main/java/com/RightDirection/ShoppingList/utils/DecimalDigitsInputFilter.java
@@ -11,7 +11,7 @@ public class DecimalDigitsInputFilter implements InputFilter {
     private final Pattern mPattern;
 
     public DecimalDigitsInputFilter(int digitsBeforeZero,int digitsAfterZero) {
-        mPattern = Pattern.compile("[0-9]*+((\\.[0-9]?)?)||(\\.)?");
+        mPattern = Pattern.compile("[0-9]{0," + (digitsBeforeZero-1) + "}+((\\.[0-9]{0," + (digitsAfterZero-1) + "})?)||(\\.)?");
     }
 
     @Override
