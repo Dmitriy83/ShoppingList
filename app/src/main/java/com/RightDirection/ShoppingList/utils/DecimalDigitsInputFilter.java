9c72af082c932461fcc15661b4a9c519ab0136f1
Dmitriy <d.zhiharev83@gmail.com>
2017-08-25 19:30:41 +0300

    Исправлены некоторые проблемы при вводе количества товара.

--- app/src/main/java/com/RightDirection/ShoppingList/utils/DecimalDigitsInputFilter.java
+++ app/src/main/java/com/RightDirection/ShoppingList/utils/DecimalDigitsInputFilter.java
@@ -6,20 +6,58 @@ import android.text.Spanned;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * Input filter that limits the number of decimal digits that are allowed to be
+ * entered.
+ */
 public class DecimalDigitsInputFilter implements InputFilter {
 
-    private final Pattern mPattern;
+    private final int decimalDigits;
 
-    public DecimalDigitsInputFilter(int digitsBeforeZero,int digitsAfterZero) {
-        mPattern = Pattern.compile("[0-9]{0," + (digitsBeforeZero-1) + "}+((\\.[0-9]{0," + (digitsAfterZero-1) + "})?)||(\\.)?");
+    /**
+     * Constructor.
+     *
+     * @param decimalDigits maximum decimal digits
+     */
+    public DecimalDigitsInputFilter(int decimalDigits) {
+        this.decimalDigits = decimalDigits;
     }
 
     @Override
-    public CharSequence filter(CharSequence source, int start, int end, Spanned dest, int dstart, int dend) {
+    public CharSequence filter(CharSequence source,
+                               int start,
+                               int end,
+                               Spanned dest,
+                               int dstart,
+                               int dend) {
 
-        Matcher matcher = mPattern.matcher(dest);
-        if(!matcher.matches())
-            return "";
+
+        int dotPos = -1;
+        int len = dest.length();
+        for (int i = 0; i < len; i++) {
+            char c = dest.charAt(i);
+            if (c == '.' || c == ',') {
+                dotPos = i;
+                break;
+            }
+        }
+        if (dotPos >= 0) {
+
+            // protects against many dots
+            if (source.equals(".") || source.equals(","))
+            {
+                return "";
+            }
+            // if the text is entered before the dot
+            if (dend <= dotPos) {
+                return null;
+            }
+            if (len - dotPos > decimalDigits) {
+                return "";
+            }
+        }
+
         return null;
     }
+
 }
